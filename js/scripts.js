04e8e7a45551c16257a2e27316eb90a527b8acd6
21-Red-code <shermang37@gmail.com>
2021-10-20 14:16:23 +0200

    The basic javascript for pokemon app
    when this change applied, it will lay the ground java script code for the Pokemon app

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'pasta';
-document.write(favoriteFood);
+let pokemonList = [];
+pokemonList = [
+    Baltoise = {ability: 'Rain-dish', speed: 45, height: 1.6},
+    Geodude = {ability: 'Rock-Headers', speed: 70, height: 0.4},
+    Machoke = { ability: 'No-guard', speed: 50, height: 1.5}
+];
