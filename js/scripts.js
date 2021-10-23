10791797acc8ec6f1f22ae9d40b17335eb775300
21-Red-code <shermang37@gmail.com>
2021-10-23 22:18:56 +0200

    Pokemon List new changes
    if this change applied, changes the font-style for body element, andchanges Pokemon List in java-script

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-let pokemonList = [];
-pokemonList = [
-    Baltoise = {ability: 'Rain-dish', speed: 45, height: 1.6},
-    Geodude = {ability: 'Rock-Headers', speed: 70, height: 0.4},
-    Machoke = { ability: 'No-guard', speed: 50, height: 1.5}
+let pokemonList = [
+    pokemon2 = {name: 'Geodude', speed: 70, height: 0.4},
+    pokemon1 = {name: 'Baltoise', speed: 45, height: 1.6},
+    pokemon3 = {name: 'Machoke', speed: 50, height: 1.5}
 ];
+
+// Lists some pokemons characters & their height and shows which one is taller
+for (let i = 0; i < pokemonList.length; i++){
+  if (pokemonList[i].height > 1.5){document.write(pokemonList[i].name  + '-------' + 'height' + ' ', pokemonList[i].height + '.' + ' '+ 'Wow that\'s big!'+ '<br>');}
+
+  else {document.write(pokemonList[i].name  + '------' + 'height' + ' ', pokemonList[i].height + '.' +  '<br>');}
+}
