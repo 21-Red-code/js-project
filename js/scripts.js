a199e3b929b16f3ed29be662bf88e55de4884d7a
21-Red-code <shermang37@gmail.com>
2021-10-29 15:16:23 +0200

    now with this new script possible to add new object in pokemonList
    when this commit applied, it makes
    the script more clean ,  will assign pokemon to IIFE variable, adds two more founction ( getAll and add ) ,updates the forEach loop so it can iterate through IIFE vaariable and possible to add new object to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
-let pokemonList = [
+let pokemonRepository = (function(){
+  let pokemonList = [
     pokemon1 = {name: 'Geodude', speed: 70, height: 0.4},
     pokemon2 = {name: 'Baltoise', speed: 45, height: 1.6},
     pokemon3 = {name: 'Machoke', speed: 50, height: 1.5}
-];
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon)
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
 
 // Lists some pokemons characters & their detail 
-pokemonList.forEach(function(pokemonDetail) {
+pokemonRepository.getAll().forEach(function(pokemonDetail) {
     document.write('Name' + ': ' + pokemonDetail.name + '<br>' + 'speed' + ': ' + pokemonDetail.speed + '<br>' + 'height' + ': ' + pokemonDetail.height + '<br>' + '<br>');
 }); 
