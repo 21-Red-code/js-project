e22bd98f52f2d15d9d966a434aa508e16edce03e
21-Red-code <shermang37@gmail.com>
2021-10-29 11:50:08 +0200

    pokemon app css and js changes
    if this commit applied, it changes:-
    font color, title of the page and in javascript changes for loop to forEach loop and also add space between each objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 let pokemonList = [
-    pokemon2 = {name: 'Geodude', speed: 70, height: 0.4},
-    pokemon1 = {name: 'Baltoise', speed: 45, height: 1.6},
+    pokemon1 = {name: 'Geodude', speed: 70, height: 0.4},
+    pokemon2 = {name: 'Baltoise', speed: 45, height: 1.6},
     pokemon3 = {name: 'Machoke', speed: 50, height: 1.5}
 ];
 
-// Lists some pokemons characters & their height and shows which one is taller
-for (let i = 0; i < pokemonList.length; i++){
-  if (pokemonList[i].height > 1.5){document.write(pokemonList[i].name  + '-------' + 'height' + ' ', pokemonList[i].height + '.' + ' '+ 'Wow that\'s big!'+ '<br>');}
-
-  else {document.write(pokemonList[i].name  + '------' + 'height' + ' ', pokemonList[i].height + '.' +  '<br>');}
-}
+// Lists some pokemons characters & their detail 
+pokemonList.forEach(function(pokemonDetail) {
+    document.write('Name' + ': ' + pokemonDetail.name + '<br>' + 'speed' + ': ' + pokemonDetail.speed + '<br>' + 'height' + ': ' + pokemonDetail.height + '<br>' + '<br>');
+}); 
