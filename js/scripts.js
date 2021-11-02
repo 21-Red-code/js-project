d3c70a75b14d9c9c1ff6fa88564fadb1ade2fa2f
21-Red-code <shermang37@gmail.com>
2021-11-02 11:15:53 +0100

    adding some styles and html element and improving the script
    when this commit applied,
    it will add  a border and heading for Pokemon list and also  changes font-color, font-family and adds boxshadow

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,33 @@ let pokemonRepository = (function(){
     pokemonList.push(pokemon)
   }
 
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = pokemon.name;
+  button.classList.add('btn-class');
+  listItem.appendChild(button);
+  pokemonList.appendChild(listItem);
+  button.addEventListener('click', function() {
+    showDetails(pokemon)
+  });
+  }//listItem
+
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
+
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem
   };
 })();
 
-// Lists some pokemons characters & their detail 
-pokemonRepository.getAll().forEach(function(pokemonDetail) {
-    document.write('Name' + ': ' + pokemonDetail.name + '<br>' + 'speed' + ': ' + pokemonDetail.speed + '<br>' + 'height' + ': ' + pokemonDetail.height + '<br>' + '<br>');
-}); 
+let pokemon = {name: 'Jargo', height: 90, speed: 40};
+(pokemonRepository.add(pokemon));
+
+pokemonRepository.getAll().forEach(function (pokemon){
+  pokemonRepository.addListItem(pokemon);
+});
